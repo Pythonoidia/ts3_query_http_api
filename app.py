507754f3ef008d9e82664aa842e3dff35134e328
krzysztofsmokowski <krzysztofsmokowski@gmail.com>
2019-01-28 22:27:01 +0100

    WIP: Add method for messages

--- app.py
+++ app.py
@@ -17,6 +17,7 @@ def establish_connection():
     ts3conn.clientupdate(client_nickname=configuration.client_nickname)
     return ts3conn
 
+conn = establish_connection()
 @auth.verify_password
 def verify_password(username, password):
     user = configuration.api_user
@@ -27,61 +28,46 @@ def verify_password(username, password):
 class Channel(Resource):
     @auth.login_required
     def get(self, cid):
-        conn = establish_connection()
         try:
             channel = conn.channelinfo(cid=cid)
         except ts3.query.TS3QueryError as msg:
             abort(404, message="{}".format(msg))
-        finally:
-            conn.quit()
         return channel.parsed, 200
 
     @auth.login_required
     def delete(self, cid):
-        conn = establish_connection()
         try:
             channel = conn.channeldelete(cid=cid, force=0)
         except ts3.query.TS3QueryError as msg:
             abort(404, message="{}".format(msg))
-        finally:
-            conn.quit()
         return None, 204
 
 
 class ClientInfo(Resource):
     @auth.login_required
     def get(self, clid):
-        conn = establish_connection()
         try:
             client = conn.clientinfo(clid=clid)
         except ts3.query.TS3QueryError as msg:
             abort(404, message="{}".format(msg))
-        finally:
-            conn.quit()
         return client.parsed
 
 class ClientList(Resource):
     @auth.login_required
     def get(self):
-        conn = establish_connection()
         try:
             clients = conn.clientlist()
         except ts3.query.TS3QueryError as msg:
             abort(404, message="{}".format(msg))
-        finally:
-            conn.quit()
         return clients.parsed
 
 class ChannelList(Resource):
     @auth.login_required
     def get(self):
-        conn = establish_connection()
         try:
             channels = conn.channellist()
         except ts3.query.TS3QueryError as msg:
             abort(404, message="{}".format(msg))
-        finally:
-            conn.quit()
         return channels.parsed
 
 class ClientMessage(Resource):
@@ -91,13 +77,10 @@ class ClientMessage(Resource):
         parser.add_argument('clid', type=int, help='clid')
         parser.add_argument('message', type=str, help='message')
         args = parser.parse_args()
-        conn = establish_connection()
         try:
             conn.sendtextmessage(targetmode=1, target=args['clid'], msg=args['message'])
         except ts3.query.TS3QueryError as msg:
             abort(404, message="{}".format(msg))
-        finally:
-            conn.quit()
         return 1,201
 
 class ChannelTopic(Resource):
@@ -107,13 +90,10 @@ class ChannelTopic(Resource):
         parser.add_argument('cid', type=int)
         parser.add_argument('channel_topic', type=str)
         args = parser.parse_args()
-        conn = establish_connection()
         try:
             conn.channeledit(cid=args['cid'], channel_topic=args['channel_topic'])
         except ts3.query.TS3QueryError as msg:
             abort(404, message="{}".format(msg))
-        finally:
-            conn.quit()
         return 1,201
 
 class ServerBanner(Resource):
@@ -125,13 +105,10 @@ class ServerBanner(Resource):
         parser = reqparse.RequestParser()
         parser.add_argument('url', type=str)
         args = parser.parse_args()
-        conn = establish_connection()
         try:
             conn.serveredit(virtualserver_hostbanner_gfx_url=args['url'])
         except ts3.query.TS3QueryError as msg:
             abort(404, message="{}".format(msg))
-        finally:
-            conn.quit()
         return 1,201
 
 
@@ -139,6 +116,41 @@ class Welcome(Resource):
     def get(self):
         return None, 200
 
+
+class Messages(Resource):
+    '''
+    '''
+    def get(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('cid', type=int)
+        parser.add_argument('message', type=str)
+        args = parser.parse_args()
+        try:
+            conn.sendtextmessage(cid=args['cid'], channel_topic=args['message'], target_mode=3)
+        except ts3.query.TS3QueryError as msg:
+            abort(404, message="{}".format(msg))
+        return 1,201
+
+
+class MessagesSubscribe(Resource):
+    '''
+    '''
+    def get(self):
+        try:
+            conn.servernotifyregister(event="textprivate")
+        except ts3.query.TS3QueryError as msg:
+            abort(404, message="{}".format(msg))
+        return None, 200
+
+class GetMessages(Resource):
+    '''
+    '''
+    def get(self):
+        try:
+            events = conn.wait_for_event(timeout=0.5)
+        except ts3.TS3TimeoutError as msg:
+            abort(404, message="{}".format(msg))
+        return events.parsed, 200
 
 
 api.add_resource(Welcome, '/')
@@ -149,7 +161,9 @@ api.add_resource(ServerBanner, '/banner')
 api.add_resource(ClientList, '/clients')
 api.add_resource(ClientInfo, '/clients/<clid>')
 api.add_resource(ClientMessage, '/clients/message')
+api.add_resource(MessagesSubscribe, '/notifyregister')
+api.add_resource(GetMessages, '/getmessages')
 
 if __name__ == '__main__':
-  http_server = WSGIServer(('', 80), app)
+  http_server = WSGIServer(('', 9998), app)
   http_server.serve_forever()
